04accfdaafb5b867f226e03015e8f93ad482e275
Lynix <lunarlynix@gmail.com>
2021-09-13 17:12:22 -0300

    Add Webcam Feature

--- ShadowmaneAI.py
+++ ShadowmaneAI.py
@@ -1,43 +1,24 @@
 import cv2
-from matplotlib import pyplot as plt
-  
-# Opening image
-img = cv2.imread("training-data.png")
-  
-# OpenCV opens images as BRG 
-# but we want it as RGB We'll 
-# also need a grayscale version
-img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-  
-  
-# Use minSize because for not 
-# bothering with extra-small 
-# dots that would look like STOP signs
-men_data = cv2.CascadeClassifier('men_data.xml')
-woman_data = cv2.CascadeClassifier('woman_data.xml')
-  
-found = men_data.detectMultiScale(img_gray, 
-                                   minSize =(20, 20))
-  
-# Don't do anything if there's 
-# no sign
-amount_found = len(found)
-  
-if amount_found != 0:
-      
-    # There may be more than one
-    # sign in the image
-    for (x, y, width, height) in found:
-          
-        # We draw a green rectangle around
-        # every recognized sign
-        cv2.rectangle(img_rgb, (x, y), 
-                      (x + height, y + width), 
-                      (0, 255, 0), 5)
-          
-# Creates the environment of 
-# the picture and shows it
-plt.subplot(1, 1, 1)
-plt.imshow(img_rgb)
-plt.show()
+import numpy as np
+
+cap = cv2.VideoCapture(0)
+
+while True:
+    ret, frame = cap.read()
+    width = int(cap.get(3))
+    height = int(cap.get(4))
+
+    image = np.zeros(frame.shape, np.uint8)
+    smaller_frame = cv2.resize(frame, (0, 0), fx=0.5, fy=0.5)
+    image[:height//2, :width//2] = cv2.rotate(smaller_frame, cv2.cv2.ROTATE_180)
+    image[height//2:, :width//2] = smaller_frame
+    image[:height//2, width//2:] = cv2.rotate(smaller_frame, cv2.cv2.ROTATE_180)
+    image[height//2:, width//2:] = smaller_frame
+
+    cv2.imshow('ShadowmaneAI - Created by Lunar Lynix', image)
+
+    if cv2.waitKey(1) == ord('q'):
+        break   
+
+cap.release()
+cv2.destroyAllWindows()
