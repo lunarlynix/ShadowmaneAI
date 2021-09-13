2b8f7c58bc5fbec3431ba06f3ce49b0210c8f4b5
Lynix <lunarlynix@gmail.com>
2021-09-13 17:44:23 -0300

    Test #2

--- ShadowmaneAI.py
+++ ShadowmaneAI.py
@@ -5,18 +5,14 @@ cap = cv2.VideoCapture(0)
 face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
 eye_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_eye.xml')
 
-ageModel="models/age_net.caffemodel"
-genderModel="models/gender_net.caffemodel"
+ageModel="age_net.caffemodel"
+genderModel="gender_net.caffemodel"
 
 ageProto="proto/age_deploy.prototxt"
 genderProto="proto/gender_deploy.prototxt"
 
 ageNet=cv2.dnn.readNet(ageModel,ageProto)
 genderNet=cv2.dnn.readNet(genderModel,genderProto)
-
-MODEL_MEAN_VALUES=(78.4263377603, 87.7689143744, 114.895847746)
-ageList=['(0-2)', '(4-6)', '(8-12)', '(15-20)', '(25-32)', '(38-43)', '(48-53)', '(60-100)']
-genderList=['Male','Female']
 
 while True:
     ret, frame = cap.read()
@@ -29,17 +25,7 @@ while True:
         cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 5)
         roi_gray = gray[y:y+w, x:x+w]
         roi_color = frame[y:y+h, x:x+w]
-        blob=cv2.dnn.blobFromImage((x, y, w, h), MODEL_MEAN_VALUES, swapRB=False)
-        genderNet.setInput(blob)
-        genderPreds=genderNet.forward()
-        gender=genderList[genderPreds[0].argmax()]
-        print(f'Gender: {gender}')
-
-        ageNet.setInput(blob)
-        agePreds=ageNet.forward()
-        age=ageList[agePreds[0].argmax()]
-        print(f'Age: {age[1:-1]} years')
-
+        print(f'Detected Face!');
         eyes = eye_cascade.detectMultiScale(roi_gray, 1.3, 5)
         for (ex, ey, ew, eh) in eyes:
             cv2.rectangle(roi_color, (ex, ey), (ex + ew, ey + eh), (0, 255, 0), 5)
