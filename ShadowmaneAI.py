6abd90961d4d41e8511de9a1121607325d7c2681
Lynix <81735347+lynixzenial@users.noreply.github.com>
2021-09-13 17:18:41 -0300

    Removed Duplicate Images

--- ShadowmaneAI.py
+++ ShadowmaneAI.py
@@ -8,14 +8,7 @@ while True:
     width = int(cap.get(3))
     height = int(cap.get(4))
 
-    image = np.zeros(frame.shape, np.uint8)
-    smaller_frame = cv2.resize(frame, (0, 0), fx=0.5, fy=0.5)
-    image[:height//2, :width//2] = cv2.rotate(smaller_frame, cv2.cv2.ROTATE_180)
-    image[height//2:, :width//2] = smaller_frame
-    image[:height//2, width//2:] = cv2.rotate(smaller_frame, cv2.cv2.ROTATE_180)
-    image[height//2:, width//2:] = smaller_frame
-
-    cv2.imshow('ShadowmaneAI - Created by Lunar Lynix', image)
+    cv2.imshow('ShadowmaneAI - Created by Lunar Lynix', frame)
 
     if cv2.waitKey(1) == ord('q'):
         break   
